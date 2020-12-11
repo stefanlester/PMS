2617071e0ff44c2f7aade424ae8c581df4a15e99
stefanlester <38575516+stefanlester@users.noreply.github.com>
2020-12-11 01:05:20 +0100

    1

---
+++ api/middleware/check-auth.js
@@ -0,0 +1,22 @@
+/* JWT ROUTE PROTECTION */
+// Check Auth to prevent unauthorized access
+// this file verifies incoming tokens from jwt
+
+/**
+ * STATELESS AUTHENTICATION
+ */
+
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.userData = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Auth failed'
+        });
+    }
+};
